2aa0d41e4bd093713559aff38d71e7d98462bb55
Vitor <vitor4ntunes@gmail.com>
2024-06-25 12:13:57 -0300

    feat: Add Sign Up page

--- src/routes/components/authRoutes.tsx
+++ src/routes/components/authRoutes.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 
 import SignIn from '../../pages/SignIn'
+import SignUp from '../../pages/SignUp'
 
 function AuthRoutes() {
   const routes = [
     {
       path: '/signin',
       component: <SignIn />
+    },
+    {
+      path: '/signup',
+      component: <SignUp />
     }
   ]
   return (
