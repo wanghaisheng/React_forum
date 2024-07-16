14fc223d2b2f022611a1ea9c4c031b2fda5ff7e8
Vitor <vitor4ntunes@gmail.com>
2024-07-16 13:49:26 -0300

    chore(firebase): Update Firebase configuration file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,7 +13,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 const provider = new GoogleAuthProvider();
 
-export { auth, provider };
+export { auth, provider, db };
